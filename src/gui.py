7ac41f6a5b10c7b581ed80be753f952be98e3ffe
Jaleksi <aleksi.jok@gmail.com>
2020-04-01 17:47:04 +0300

    change node color on mouse hover

--- src/gui.py
+++ src/gui.py
@@ -1,16 +1,19 @@
+# Standard
 import os
 
+# 3rd party
 from PyQt5 import QtWidgets, QtGui, QtCore
 
+# Local
 from .node_file import Node
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setMouseTracking(True)
         self.setWindowTitle('kauppa_router')
         self.canvas = Canvas()
+        self.canvas.setMouseTracking(True)
         self.setCentralWidget(self.canvas)
         self.init_menus()
 
@@ -38,7 +41,7 @@ class MainWindow(QtWidgets.QMainWindow):
         dialog = QtWidgets.QFileDialog(self)
         img_path, _ = dialog.getOpenFileName(self, "Load image", "")
         if os.path.exists(img_path):
-            self.canvas.new_route(img_path)
+            self.canvas.new_image(img_path)
             self.resize(self.canvas.x() + self.canvas.image.width(),
                         self.canvas.y() + self.canvas.image.height())
 
@@ -57,29 +60,36 @@ class Canvas(QtWidgets.QLabel):
                 return
             self.mode = mode
 
-    def new_route(self, img):
+    def new_image(self, img):
         self.image = QtGui.QPixmap(img)
         self.update()
 
     def paintEvent(self, event):
         if not self.image:
             return
+
         p = QtGui.QPainter(self)
         p.drawPixmap(self.rect(), self.image)
+        pen = QtGui.QPen()
+        pen.setWidth(10)
+
+        for node in self.nodes:
+            r, g, b = node.color
+            pen.setColor(QtGui.QColor(r, g, b))
+            p.setPen(pen)
+            p.drawPoint(node.x, node.y)
 
     def mousePressEvent(self, event):
         if event.buttons() == QtCore.Qt.LeftButton and self.mode == 'node_edit':
-            p = QtGui.QPainter(self.image)
-            self.drawPoint(event, p)
+            self.nodes.append(Node(event.pos().x(), event.pos().y(), (0, 0, 255)))
             self.update()
 
     def mouseMoveEvent(self, event):
-        pass
-
-    def drawPoint(self, event, p):
-        pen = QtGui.QPen(QtCore.Qt.green)
-        pen.setWidth(10)
-        p.setPen(pen)
-        self.nodes.append(Node(event.pos().x(), event.pos().y()))
-        p.drawPoint(event.pos())
-        print([(n.x, n.y) for n in self.nodes])
+        if self.mode == 'node_edit':
+            mx, my = event.pos().x(), event.pos().y()
+            for node in self.nodes:
+                if node.within_reach(mx, my):
+                    node.color = (0, 255, 0)
+                else:
+                    node.color = (0, 0, 255)
+            self.repaint()
--- src/node_file.py
+++ src/node_file.py
@@ -1,6 +1,12 @@
+import math
+
+
 class Node:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.pairs = []
+        self.color = color
 
+    def within_reach(self, x, y):
+        return math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2) < 10
