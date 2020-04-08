ffdde6128cfef9fa5eab0204aee782b63fd0a214
Jaleksi <aleksi.jok@gmail.com>
2020-04-08 17:02:50 +0300

    use float instead of int

--- src/path_utils/path_manager.py
+++ src/path_utils/path_manager.py
@@ -26,8 +26,8 @@ class PathManager:
             assert node.connects_with, 'Found node without connections, exiting.'
             for connection in node.connects_with:
                 # Continue if connection already in list from other node
-                #if (node, connection)[::-1] in pairs:
-                #    continue
+                if (node, connection)[::-1] in pairs:
+                    continue
                 pairs.append((node, connection))
 
         # Add distances
@@ -35,7 +35,7 @@ class PathManager:
         for con in pairs:
             p1, p2 = con[0], con[1]
             distance = points_distance((p1.x, p1.y), (p2.x, p2.y))
-            connections.append(con + (int(distance), ))
+            connections.append(con + (distance, ))
         return connections
 
     def get_shortest_route(self):
